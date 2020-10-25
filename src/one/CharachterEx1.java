4cb68dd98f627e72c47be311d36a469c7f2b530b
user <user@DESKTOP-KJUO3L6>
2020-10-26 00:25:47 +0900

    문자형 연습

---
+++ src/one/CharachterEx1.java
@@ -0,0 +1,18 @@
+package one;
+
+public class CharachterEx1 {
+
+	public static void main(String[] args) {
+		char ch1 ='A';
+		System.out.print(ch1); //문자 줄력
+		System.out.print((int)ch1); //문자에 해당하는 아스키코드 출력
+		
+		char ch2 =66; //정수 값 대입
+		System.out.print(ch2); //정수 값에 해당하는 문자 출력
+		
+		int ch3 =67;
+		System.out.print(ch3); // 문자 정수 값 출력
+		System.out.print((char)ch3); //정수 값에 해당하는 문자 출력		
+	}
+
+}
